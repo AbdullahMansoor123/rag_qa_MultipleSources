5eb598053060a24aaa5e291eed088d47702eb720
AbdullahMansoor123 <abdullahmansoor123@gmail.com>
2024-09-29 15:24:12 +0500

    1_version

--- app.py
+++ app.py
@@ -10,127 +10,116 @@ from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
 from langchain_openai import ChatOpenAI
 from langchain.tools.retriever import create_retriever_tool
 from langchain.agents import create_openai_tools_agent
 from langchain.agents import AgentExecutor
-
-
-
 import re
 
-def isValidURL(url):
+# Function to validate a URL using regex
+def is_valid_url(url: str) -> bool:
     regex = r"((http|https)://)(www\.)?[a-zA-Z0-9@:%._\+~#?&//=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%._\+~#?&//=]*)"
     return True if re.match(regex, url) else False
 
-def wiki_tool():
-    api_wrapper=WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=250)
-    wiki = WikipediaQueryRun(api_wrapper=api_wrapper)
-    return wiki
+# Function to create a Wikipedia tool using the WikipediaAPIWrapper
+def wiki_tool() -> WikipediaQueryRun:
+    api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=250)
+    return WikipediaQueryRun(api_wrapper=api_wrapper)
 
-
-def website_tool(url):
+# Function to create a website retriever tool from a given URL
+def website_tool(url: str):
     loader = WebBaseLoader(url)
     docs = loader.load()
-    documents = RecursiveCharacterTextSplitter(chunk_size =1000, chunk_overlap = 200).split_documents(docs)
+    documents = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200).split_documents(docs)
     vectordb = FAISS.from_documents(documents, OpenAIEmbeddings())
     retriever = vectordb.as_retriever()
-    retriever_tool = create_retriever_tool(retriever, "world-best-cities","Guide on best cities in the world")
-    return retriever_tool
+    return create_retriever_tool(retriever, "world-best-cities", "Guide on best cities in the world")
 
-# get all the pdf in a single document
-def get_pdf_text(pdf_docs):
+# Function to extract text from a list of PDF files
+def get_pdf_text(pdf_docs) -> str:
     text = ""
     for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf,strict=False)
+        pdf_reader = PdfReader(pdf, strict=False)
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
 
-# get text chunks
-def get_text_chunks(docs):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size =1000, chunk_overlap = 200)
-    chunks = text_splitter.split_text(docs)
-    return chunks
+# Function to split text into chunks for processing
+def get_text_chunks(text: str):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    return text_splitter.split_text(text)
 
-#get text embedding
-def get_vector_store(chunks):
+# Function to create a vector store using text embeddings
+def get_vector_store(chunks: list) -> FAISS:
     embedding = OpenAIEmbeddings()
-    vectordb = FAISS.from_texts(chunks, embedding)
-    return vectordb
+    return FAISS.from_texts(chunks, embedding)
 
+# Function to create a retriever tool for PDF content
 def pdf_retriever_tool(pdf_path):
     pdf_text = get_pdf_text(pdf_path)
     chunks = get_text_chunks(pdf_text)
     vector_db = get_vector_store(chunks)
-    pdf_retriever =  vector_db.as_retriever()
-    return create_retriever_tool(pdf_retriever, "pdf_tool","PDF Content")
+    return create_retriever_tool(vector_db.as_retriever(), "pdf_tool", "PDF Content")
 
-# llm response
-def llm_response(user_question,selected_sources):
-
+# Function to generate an LLM response based on the selected sources
+def llm_response(user_question: str, selected_sources: list, openai_api_key: str) -> str:
     tools = []
+
+    # Sidebar options for data sources
     with st.sidebar:
         if "Wikipedia" in selected_sources:
-            wiki = wiki_tool()
-            tools.append(wiki)
+            tools.append(wiki_tool())
         
-        uploaded_file = None
-        if "PDF" in selected_sources :
-            # Upload document option
+        if "PDF" in selected_sources:
             uploaded_files = st.file_uploader("Upload a document (PDF):", type=['pdf'], accept_multiple_files=True)
-            if uploaded_file is not None:
+            if uploaded_files:
                 with st.spinner("Processing..."):
-                    pdf = pdf_retriever_tool(uploaded_files)
-                    tools.append(pdf)
+                    tools.append(pdf_retriever_tool(uploaded_files))
             else:
-                st.write("Please make sure to select a pdf.")
+                st.write("Please make sure to select a PDF file.")
         
-        if "Website" in selected_sources :
+        if "Website" in selected_sources:
             url = st.text_input("Enter website URL:")
-            if isValidURL(url) == False :
+            if not is_valid_url(url):
                 st.warning("Please enter a valid URL to proceed.")
             else:
-                website = website_tool(url)
-                tools.append(website)
+                tools.append(website_tool(url))
 
-    #select llm model
-    gpt = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-
-    ## get the prompt from hub
+    # Select the LLM model with the provided OpenAI API key
+    gpt = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, openai_api_key=openai_api_key)
     prompt = hub.pull("hwchase17/openai-functions-agent")
-
     my_agent = create_openai_tools_agent(gpt, tools, prompt)
-    agent_executer  = AgentExecutor(agent=my_agent, tools= tools, verbose=False)
-    
-    answer = agent_executer.invoke({
-            "input": {user_question}
-            })
+    agent_executor = AgentExecutor(agent=my_agent, tools=tools, verbose=False)
+
+    # Get the response from the LLM agent
+    answer = agent_executor.invoke({"input": user_question})
     return answer['output']
-  
 
+# Main function to set up the Streamlit app interface
 def main():
-    try:
-        st.title("Question-Answering System")
-        # Input box for user question
-        user_question = st.text_input("Ask your question:")
+    st.title("Question-Answering System")
+
+    # Input box for user question
+    user_question = st.text_input("Ask your question:")
+
+    # Sidebar options for selecting data sources and inputting the OpenAI API key
+    with st.sidebar:
+        # Input for OpenAI API key
+        openai_api_key = st.text_input("Enter your OpenAI API Key:", type="password")
+
+        st.subheader("More Options")
+        sources = ["Wikipedia", "PDF", "Website"]
+        selected_sources = st.multiselect("Select data sources:", options=sources, default="Wikipedia")
         
         
-        with st.sidebar:
-            st.subheader("More Options")
-            # Multi-select for data sources
-            sources = ["Wikipedia", "PDF", "Website"]
-            selected_sources = st.multiselect("Select data sources:", options=sources,default="Wikipedia")
 
-        # Process the question and display the response
-        if user_question and selected_sources:
-            answer = llm_response(user_question, selected_sources)
-            st.write(answer)
+    # Process the question and display the response
+    if user_question and selected_sources and openai_api_key:
+        answer = llm_response(user_question, selected_sources, openai_api_key)
+        st.write(answer)
+    else:
+        st.write("Please provide all inputs (question, data sources, and OpenAI API key).")
 
-    except:
-        pass
-
-
+# Entry point of the Streamlit app
 if __name__ == '__main__':
     main()
